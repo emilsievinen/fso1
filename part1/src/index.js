860cc420a4cf894f36e108ccb5957fa10c34bf19
Emil Sievinen <emilsievinen@gmail.com>
2019-12-14 11:40:21 +0200

    final practice

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
-
+const Hello = (props) => {
     return (
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-    </div>
+      <div>
+        <p>
+          Hello {props.name}, you are {props.age} years old
+        </p>
+      </div>
     )
-}
+  }
+  
+const Footer = () => {
+    return (
+      <div>
+        greeting app created by
+        <a href="https://github.com/mluukkai">mluukkai</a>
+      </div>
+    )
+  }
+
+const App = () => {
+    const name = 'Pekka'
+    const age = 10
+  
+    return (
+      <>
+        <h1>Greetings</h1>
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={name} age={age} />
+        <Footer />
+      </>
+    )
+  }
+  
 
 ReactDOM.render(<App />, document.getElementById('root'))
