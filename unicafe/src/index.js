eaf89bf45161a0198af3d6c58d7c58872aa2f2ff
Emil Sievinen <emilsievinen@gmail.com>
2019-12-21 12:44:44 +0200

    ei toimi

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Header />
+      <Counters good= {good, setGood} neutral = {neutral, setNeutral} bad = {bad, setBad}/>
+    </div>
+  )
+}
+
+const Header = () => {
+    return (
+        <>
+        <h1>
+            Unicafe feedback
+        </h1>
+        </>
+    )
+}
+
+const Counters = (props) => {
+  return (
+      <>
+      <h3>
+        Give feedback here!
+      </h3>
+      <Button
+        onClick={() => props.good.setGood(props.good.good + 1)}
+        text='positive'
+      />
+      <Button
+        onClick={() => props.bad.setBad(props.bad.bad + 1)}
+        text='negative'
+      />
+      <Button
+        onClick={() => props.neutral.setNeutral(props.neutral.neutral + 1)}
+        text='enemmän lihaa'
+      />
+      </>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
